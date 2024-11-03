3649e0c819c0b7db51b6acb5d7d3318dbe8fb268
aaditx23 <adit.pranto@gmail.com>
2024-11-03 16:06:19 +0600

    Feat: Icon (AI Generated)

--- app/src/main/java/com/aaditx23/wallpaperwizard/ui/screens/QuickSetScreen.kt
+++ app/src/main/java/com/aaditx23/wallpaperwizard/ui/screens/QuickSetScreen.kt
@@ -1,15 +1,21 @@
 package com.aaditx23.wallpaperwizard.ui.screens
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DeleteSweep
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
@@ -35,13 +41,19 @@ fun QuickSetScreen(
             modifier = Modifier
                 .padding(top = 60.dp)
         ){
-            Button(
-                onClick = croppedFolder,
-                modifier = Modifier
-                    .fillMaxWidth(),
-                shape = RectangleShape
-            ) {
-                Text("Cropped Pictures Cache")
+            Box(
+              modifier = Modifier
+                  .fillMaxWidth(),
+                contentAlignment = Alignment.TopEnd
+            ){
+                IconButton(
+                    onClick = croppedFolder,
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.DeleteSweep,
+                        contentDescription = "Cche"
+                    )
+                }
             }
             if(allQuickSets.isEmpty()){
                 EmptyScreenText("No QuickSets")
