38df6e90362af78534089cb75dd51100a5b32a03
aaditx23 <adit.pranto@gmail.com>
2025-08-17 02:10:55 +0600

    Chore: Kotlin, KSP, realm version upgrade for 16kb page

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
 
-    id("io.realm.kotlin") version ("2.0.0") apply false
+    id("io.realm.kotlin") version ("2.3.0") apply false
     id("com.google.dagger.hilt.android") version ("2.49") apply false
-    id("com.google.devtools.ksp") version "2.0.0-1.0.24" apply false
+    id("com.google.devtools.ksp") version "2.0.20-1.0.24" apply false
 }
